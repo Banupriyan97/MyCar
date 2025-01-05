f8610afed0cee1ad1db12dacf8017c78458562f0
Banupriyan <banuraveendran@gmail.com>
2025-01-05 14:25:35 +0530

    Implement CRUD operations

---
+++ MyCar.Server/Controllers/Resources/VehicleResource.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.VisualBasic;
+using MyCar.Server.Models;
+
+namespace MyCar.Server.Controllers.Resources
+{
+
+    public class VehicleResource
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public bool IsRegistered { get; set; }
+        public ContactResource Contact { get; set; }
+        public ICollection<int> Features { get; set; }
+
+        public VehicleResource()
+        {
+            Features = new Collection<int>();
+        }
+    }
+}
--- MyCar.Server/Persistence/MyCarDbContext.cs
+++ MyCar.Server/Persistence/MyCarDbContext.cs
@@ -5,13 +5,21 @@ namespace MyCar.Server.Persistence
 {
     public class MyCarDbContext: DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Make> Models { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        
         public MyCarDbContext(DbContextOptions<MyCarDbContext> options): base(options)
         {
             
         }
 
-        public DbSet<Make> Makes { get; set; }
-        public DbSet<Feature> Features { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf =>
+            new { vf.VehicleId, vf.FeatureId });
+        }
     }
 }
  
